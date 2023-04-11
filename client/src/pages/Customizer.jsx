d7d367173bc9a1562d0b983677f52447ce386857
Victor Ribeiro <contato.victoribeiro@gmail.com>
2023-04-10 22:12:09 -0300

    ThreeJS Camera

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -38,7 +38,7 @@ const Customizer = () => {
           </motion.div>
 
           <motion.div
-            className='absolute z-10 top5 right-5'
+            className="absolute z-10 top-5 right-5"
             {...fadeAnimation}
           >
             <CustomButton 
